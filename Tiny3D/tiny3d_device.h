93c87197e66c46ee26b47f682e231e45dfb06cbe
kumakoko <sun_of_lover@sina.com>
2024-10-20 16:36:49 +0800

    更新tiny3D代码

---
+++ Tiny3D/tiny3d_device.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+
+#include <cstdint>
+
+#include "tiny3d_transform.h"
+#include "tiny3d_geometry.h"
+
+//=====================================================================
+// 渲染设备
+//=====================================================================
+
+#define RENDER_STATE_WIREFRAME      1		// 渲染线框
+#define RENDER_STATE_TEXTURE        2		// 渲染纹理
+#define RENDER_STATE_COLOR          4		// 渲染颜色
+
+
+struct device_t {
+    transform_t transform;      // 坐标变换器
+    int width;                  // 窗口宽度
+    int height;                 // 窗口高度
+    uint32_t** framebuffer;      // 像素缓存：framebuffer[y] 代表第 y行
+    float** zbuffer;            // 深度缓存：zbuffer[y] 为第 y行指针
+    uint32_t** texture;          // 纹理：同样是每行索引
+    int tex_width;              // 纹理宽度
+    int tex_height;             // 纹理高度
+    float max_u;                // 纹理最大宽度：tex_width - 1
+    float max_v;                // 纹理最大高度：tex_height - 1
+    int render_state;           // 渲染状态
+    uint32_t background;         // 背景颜色
+    uint32_t foreground;         // 线框颜色
+public:
+
+
+    // 设备初始化，fb为外部帧缓存，非 NULL 将引用外部帧缓存（每行 4字节对齐）
+    void device_init(int width, int height, void* fb);
+
+    // 删除设备
+    void device_destroy();
+
+    // 设置当前纹理
+    void device_set_texture(void* bits, long pitch, int w, int h);
+
+    // 清空 framebuffer 和 zbuffer
+    void device_clear(int mode);
+
+    // 画点
+    void device_pixel(int x, int y, uint32_t color);
+
+    // 绘制线段
+    void device_draw_line(int x1, int y1, int x2, int y2, uint32_t c);
+
+    // 根据坐标读取纹理
+    uint32_t device_texture_read(float u, float v);
+
+    // 绘制扫描线
+    void device_draw_scanline(scanline_t* scanline);
+
+    // 主渲染函数
+    void device_render_trap(trapezoid_t* trap);
+
+    // 根据 render_state 绘制原始三角形
+    void device_draw_primitive(const vertex_t* v1, const vertex_t* v2, const vertex_t* v3);
+};
+
---
+++ Tiny3D/tiny3d_geometry.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+
+#include "tiny3d_vector.h"
+
+//=====================================================================
+// 几何计算：顶点、扫描线、边缘、矩形、步长计算
+//=====================================================================
+
+struct color_t
+{
+    float r, g, b;
+};
+
+struct texcoord_t
+{
+    float u, v;
+};
+
+struct vertex_t
+{
+    point_t pos;
+    texcoord_t tc;
+    color_t color;
+    float rhw;
+};
+
+struct edge_t
+{
+    vertex_t v, v1, v2;
+};
+
+struct trapezoid_t
+{
+    float top, bottom; edge_t left, right;
+};
+
+struct scanline_t
+{
+    vertex_t v, step;
+    int x, y, w;
+};
+
+void vertex_rhw_init(vertex_t* v);
+
+void vertex_interp(vertex_t* y, const vertex_t* x1, const vertex_t* x2, float t);
+
+void vertex_division(vertex_t* y, const vertex_t* x1, const vertex_t* x2, float w);
+
+void vertex_add(vertex_t* y, const vertex_t* x);
+
+// 根据三角形生成 0-2 个梯形，并且返回合法梯形的数量
+int trapezoid_init_triangle(trapezoid_t* trap, const vertex_t* p1, const vertex_t* p2, const vertex_t* p3);
+
+// 按照 Y 坐标计算出左右两条边纵坐标等于 Y 的顶点
+void trapezoid_edge_interp(trapezoid_t* trap, float y);
+
+// 根据左右两边的端点，初始化计算出扫描线的起点和步长
+void trapezoid_init_scan_line(const trapezoid_t* trap, scanline_t* scanline, int y);
